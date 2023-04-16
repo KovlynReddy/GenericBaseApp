36e65b6cc5a04cb8ee56b6a9ae153b2bec8eabdb
Kovlyn <kovlyn.reddy@gmail.com>
2023-04-16 16:02:35 +0200

    ChangeThemes

--- GenericBaseMVC/wwwroot/js/site.js
+++ GenericBaseMVC/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+function ChangeTheme(themeName) {
+var theme = $("#theme");
+var newUrl = `/lib/bootstrap-themes/${themeName}/bootstrap.css`
+console.log(newUrl, theme.attr('href'));
+theme.attr('href', newUrl);
+}
 
-// Write your JavaScript code.
+
+
+ChangeTheme("Candy");
