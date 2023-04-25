6c93921abe633050a49ab0d47fdfffd830827f9b
Kovlyn <kovlyn.reddy@gmail.com>
2023-04-25 22:55:41 +0200

    ApiInteractionControllerBase

--- GenericBaseMVC/Controllers/MeetupRequestController.cs
+++ GenericBaseMVC/Controllers/MeetupRequestController.cs
@@ -1,38 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace VendorAPI.Controllers
+namespace GenericBaseMVC.Controllers
 {
-    [Route("api/MeetupRequest")]
-    [ApiController]
     public class MeetupRequestController : Controller
     {
-        [HttpGet]
-        [Route("~/api/MeetupRequest")]
         public async Task<IActionResult> Get()
         {
             return View();
         }
-
-        [HttpGet]
-        [Route("~/api/MeetupRequest/{id}")]
         public async Task<IActionResult> Get(string id)
         {
             return View();
         }
-
-        [HttpPost]
-        [Route("~/api/MeetupRequest")]
         public async Task<IActionResult> Post()
         {
             return View();
         }
-
-        [HttpPut]
-        [Route("~/api/MeetupRequest")]
         public async Task<IActionResult> Put()
         {
             return View();
         }
-
     }
 }
--- VendorAPI/Controllers/PostInteractionController.cs
+++ VendorAPI/Controllers/PostInteractionController.cs
@@ -2,20 +2,33 @@
 
 namespace VendorAPI.Controllers
 {
+    [Route("api/PostInteraction")]
+    [ApiController]
     public class PostInteractionController : Controller
     {
+        [HttpGet]
+        [Route("~/api/PostInteraction")]
         public async Task<IActionResult> Get()
         {
             return View();
         }
+
+        [HttpGet]
+        [Route("~/api/PostInteraction/{id}")]
         public async Task<IActionResult> Get(string id)
         {
             return View();
         }
+
+        [HttpPost]
+        [Route("~/api/PostInteraction")]
         public async Task<IActionResult> Post()
         {
             return View();
         }
+
+        [HttpPut]
+        [Route("~/api/PostInteraction")]
         public async Task<IActionResult> Put()
         {
             return View();
