63ebd58588ac9667c6763bd7e93840c865d5bad8
Kovlyn <62847853+KovlynReddy@users.noreply.github.com>
2023-10-10 16:24:27 +0100

    MinorUI

--- GenericAppDLL/Models/DomainModel/DM.cs
+++ GenericAppDLL/Models/DomainModel/DM.cs
@@ -2,13 +2,13 @@
 
 public class DM : BaseModel
 {
-    public string SenderGuid { get; set; }            = string.Empty;
-    public string RecieverGuid { get; set; }          = string.Empty;
-    public string GroupGuid { get; set; }             = string.Empty;
-    public string Message { get; set; }               = string.Empty;
-    public string AttatchmentString { get; set; }     = string.Empty;
-    public string Path { get; set; }                  = string.Empty;
-    public string Status { get; set; }                = string.Empty;
+    public string SenderGuid { get; set; } = string.Empty;
+    public string RecieverGuid { get; set; } = string.Empty;
+    public string GroupGuid { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public string AttatchmentString { get; set; } = string.Empty;
+    public string Path { get; set; } = string.Empty;
+    public string Status { get; set; } = string.Empty;
     public int Read { get; set; }
 
 }
